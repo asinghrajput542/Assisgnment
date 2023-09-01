c898e211ffb05328fb13c48da1217dab321d4bbf
Ayush Singh <asinghrajput542@gmail.com>
2023-09-01 11:21:16 +0530

    stop/reset functionality

--- dom-manipulation/vanilla-stop-watch/index.js
+++ dom-manipulation/vanilla-stop-watch/index.js
@@ -1,35 +1,64 @@
 let timeInterval;
-const action = start.addEventListener("click", () => {
-  start.setAttribute("disabled", true);
-  let secCount = 57;
-  let minCount = 59;
-  let hrsCount = 0;
+stopBtn.setAttribute("disabled", true);
+resetBtn.setAttribute("disabled", true);
+let secCount = 57;
+let minCount = 59;
+let hrsCount = 0;
+const startAction = startBtn.addEventListener("click", () => {
+  startBtn.setAttribute("disabled", true);
+  stopBtn.removeAttribute("disabled");
+  resetBtn.removeAttribute("disabled");
+
   timeInterval = setInterval(() => {
     secCount += 1;
-    const newDiv = document.createElement("div");
-    newDiv.className = "sec";
-    newDiv.id = "sec";
-    newDiv.append(secCount);
-    sec.replaceWith(newDiv);
+    const secDiv = divCreator("sec", "sec", secCount);
+    sec.replaceWith(secDiv);
 
     if (secCount === 59) {
       secCount = -1;
       if (minCount === 59) {
         minCount = -1;
-        const hrsDiv = document.createElement("div");
-        hrsDiv.className = "hrs";
-        hrsDiv.id = "hrs";
         hrsCount += 1;
-        hrsDiv.append(hrsCount);
+        const hrsDiv = divCreator("hrs", "hrs", hrsCount);
         hrs.replaceWith(hrsDiv);
       }
-
-      const minDiv = document.createElement("div");
-      minDiv.className = "min";
-      minDiv.id = "min";
       minCount += 1;
-      minDiv.append(minCount);
+      const minDiv = divCreator("min", "min", minCount);
       min.replaceWith(minDiv);
     }
   }, 1000);
 });
+
+const stopAction = stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute("disabled");
+  stopBtn.setAttribute("disabled", true);
+  if (timeInterval) {
+    clearInterval(timeInterval);
+  }
+});
+
+const resetAction = resetBtn.addEventListener("click", () => {
+  if (timeInterval) {
+    clearInterval(timeInterval);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", true);
+    resetBtn.setAttribute("disabled", true);
+    secCount = 0;
+    minCount = 0;
+    hrsCount = 0;
+    const hrsDiv = divCreator("hrs", "hrs", hrsCount);
+    hrs.replaceWith(hrsDiv);
+    const secDiv = divCreator("sec", "sec", secCount);
+    sec.replaceWith(secDiv);
+    const minDiv = divCreator("min", "min", minCount);
+    min.replaceWith(minDiv);
+  }
+});
+
+const divCreator = (divClassName, divId, value) => {
+  const newDiv = document.createElement("div");
+  newDiv.className = divClassName;
+  newDiv.id = divId;
+  newDiv.append(value);
+  return newDiv;
+};
