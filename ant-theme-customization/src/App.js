c0a5fa66db17c6701c03cfaf126138f43eda7d2d
Ayush Singh <asinghrajput542@gmail.com>
2023-09-14 18:41:33 +0530

    antd theme configuration

---
+++ ant-theme-customization/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Input, Radio, Checkbox } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+      <Button>Default</Button>
+
+      <Input placeholder="Please Input" />
+      <Button type="primary">Submit</Button>
+
+      <Radio.Group defaultValue="a" size="small" buttonStyle="solid">
+        <Radio.Button value="a">Hangzhou</Radio.Button>
+        <Radio.Button value="b">Shanghai</Radio.Button>
+        <Radio.Button value="c">Beijing</Radio.Button>
+        <Radio.Button value="d">Chengdu</Radio.Button>
+      </Radio.Group>
+      <Checkbox>Checkbox</Checkbox>
+    </div>
+  );
+}
+
+export default App;
---
+++ ant-theme-customization/src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token global like hover
+          colorPrimary: "#794AFE",
+          // colorText: "#794AFE",
+          // borderRadius: 10,
+          // Alias Token
+          // colorBgContainer: "#EDE2FF",
+        },
+        components: {
+          Input: {
+            // colorPrimary: "red",
+            // algorithm: true,
+          },
+          Button: {
+            colorPrimary: "#794AFE",
+          },
+          Radio: {
+            colorPrimary: "#EDE2FF",
+            algorithm: true,
+          },
+          Checkbox: {
+            borderRadius: 50,
+            algorithm: true,
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
